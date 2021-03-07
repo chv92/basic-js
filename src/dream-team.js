4a8262089a0fc66d614051eafffbb350ab325618
chv92 <vitalii.ch92@gmail.com>
2021-03-07 17:51:18 +0100

    Finish carbon dream season

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let str = "";
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  for (let i = 0; i < members.length; i++) {
+    if (typeof (members[i]) === 'string') {
+      let name = members[i].trim()
+      str += name[0];
+    }
+  }
+
+
+  return str.toUpperCase().split("").sort().join("");
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+
+
+  season = date.getUTCMonth();
+
+  if (season == '0' || season == '1' || season == '11') {
+    return 'winter'
+  }
+  if (season >= '2' && season <= '4') {
+    return 'spring';
+  }
+
+  if (season >= 5 && season <= 7) {
+    return 'summer';
+  }
+
+  if (season >= 8 && season <= 10) {
+    return "fall"
+  }
 };
