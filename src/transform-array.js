137519ef3c4bb5d137ea5bef4111445f74c0ca00
chv92 <vitalii.ch92@gmail.com>
2021-03-29 18:54:30 +0200

    fix: transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -8,31 +8,39 @@ module.exports = function transform(arr) {
     let discardPrev = '--discard-prev';
     let doubleNext = '--double-next';
     let doublePrew = '--double-prev';
-    if (arr1.indexOf(discardNext) != -1) {
-      arr1.splice(arr1.indexOf(discardNext), (arr1.indexOf(discardNext)) + 1);
-    }
-    else if (arr1.indexOf(discardPrev) != -1) {
-      if (arr1.indexOf(discardPrev) == 0) {
-        arr1.splice(arr1.indexOf(discardPrev));
-      } else {
-        arr1.splice(arr1.indexOf(discardPrev) - 1);
+    for( let i =0 ; i< arr1.length; i++){
+      if (arr1.indexOf(discardNext) != -1) {
+        if(arr1.indexOf(discardNext) == arr1.length-1) {
+          arr1.splice(arr1.indexOf(discardNext),2);
+        } else{
+          arr1.splice((arr1.indexOf(discardNext))-1,2);
+        } 
       }
-    }
-    else if (arr1.indexOf(doubleNext) != -1) {
-      arr1[arr1.indexOf(doubleNext)] = arr1[arr1.indexOf(doubleNext) + 1];
-      // arr1.splice(arr1.indexOf(doubleNext));Q
-    }
-    else if (arr1.indexOf(doublePrew) != -1) {
-      if (arr1.indexOf(doublePrew) == 0) {
-        arr1.splice(arr1.indexOf(doublePrew));
-      } else {
-        arr1[arr1.indexOf(doublePrew)] = arr1[(arr1.indexOf(doublePrew)) - 1];
+      else if (arr1.indexOf(discardPrev) != -1) {
+        if (arr1.indexOf(discardPrev) == 0) {
+          arr1.splice(arr1.indexOf(discardPrev),1);
+        } else {
+          arr1.splice(arr1.indexOf(discardPrev) - 1,2);
+        }
       }
-      // arr1.splice(arr1.indexOf(doubleNext));
+      else if (arr1.indexOf(doubleNext) != -1) {
+        if(arr1.indexOf(doubleNext) == arr1.length -1 ){
+          arr1.splice(arr1.indexOf(doubleNext),2);
+        }else{
+          arr1[arr1.indexOf(doubleNext)] = arr1[arr1.indexOf(doubleNext) + 1];
+        }
+
+      }
+      else if (arr1.indexOf(doublePrew) != -1) {
+        if (arr1.indexOf(doublePrew) == 0) {
+          arr1.splice(arr1.indexOf(doublePrew),1);
+        } else {
+          arr1[arr1.indexOf(doublePrew)] = arr1[(arr1.indexOf(doublePrew)) - 1];
+        }
+      }
+      
     }
     return arr1;
-
+   
   } else return error();
-
-
 };
